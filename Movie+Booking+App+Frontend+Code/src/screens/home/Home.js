20358963a7835732c9086b97fbbb703c333f0559
praneeth <praneethr1998@gmail.com>
2021-07-22 00:05:54 +0530

    checkpoint 10

--- Movie+Booking+App+Frontend+Code/src/screens/home/Home.js
+++ Movie+Booking+App+Frontend+Code/src/screens/home/Home.js
@@ -319,7 +319,18 @@ class Home extends Component {
                     onChange={this.releaseDateEndHandler}
                   />
                 </FormControl>
-
+                <FormControl className={classes.formControl}>
+                  <TextField
+                    id="releaseDateEnd"
+                    label="Release Date End"
+                    type="date"
+                    defaultValue=""
+                    InputLabelProps={{ shrink: true }}
+                    onChange={this.releaseDateEndHandler}
+                  />
+                </FormControl>
+                <br />
+                <br />
                 <FormControl className={classes.formControl}>
                   <Button
                     onClick={() => this.filterApplyHandler()}
